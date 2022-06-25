a5773505bbb4e2a4275413367b122e1d6e92ea61
Tai Helsel <33849433+taihelsel@users.noreply.github.com>
2022-06-25 18:10:47 -0400

    Added home text styles

--- src/Sections/Home/Home.js
+++ src/Sections/Home/Home.js
@@ -10,8 +10,10 @@ function Home() {
     return (
         <section id="Home">
             <div className="home-left">
-                <h1 className="home-title">Hi, I'm <h1 id="home-firstname" className="home-title">Tai</h1></h1>
-                <h2 className="home-title">I'm creating with Node.js</h2>
+                <div id="home-head-container">
+                    <h1 className="home-title home-head">Hi, I'm </h1><h1 className="home-title home-head" id="home-firstname">&nbsp;Tai</h1>
+                </div>
+                <h2 className="home-title home-subhead">I'm creating with Node.js</h2>
                 <p className="home-body"> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it</p>
             </div>
             <div className="home-right">
