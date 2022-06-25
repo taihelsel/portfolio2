aa564c9f2714a09e6c043ccba3957087ca984a2b
Tai Helsel <33849433+taihelsel@users.noreply.github.com>
2022-06-25 17:54:27 -0400

    stubbed out home section

--- src/Sections/Home/Home.js
+++ src/Sections/Home/Home.js
@@ -1,8 +1,23 @@
-function Home() {
+import "./Home.css";
+function Terminal() {
     return (
         <div>
-            stuff
+            <h1>Terminal</h1>
         </div>
+    );
+}
+function Home() {
+    return (
+        <section id="Home">
+            <div className="home-left">
+                <h1 className="home-title">Hi, I'm <h1 id="home-firstname" className="home-title">Tai</h1></h1>
+                <h2 className="home-title">I'm creating with Node.js</h2>
+                <p className="home-body"> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it</p>
+            </div>
+            <div className="home-right">
+                <Terminal />
+            </div>
+        </section>
     );
 }
 export default Home;
