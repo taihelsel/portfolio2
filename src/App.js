03cd8de53ced03619eca79ce7502980b64ee33d0
Tai Helsel <33849433+taihelsel@users.noreply.github.com>
2022-06-27 08:29:09 -0400

    added Experience component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Nav from './Components/Nav/Nav';
 import Home from "./Sections/Home/Home.js";
+import Experience from './Sections/Experience/Experience';
 function App() {
   return (
     <div className="App">
       <Nav />
       <Home />
+      <Experience />
     </div>
   );
 }
---
+++ src/Sections/Experience/Experience.js
@@ -0,0 +1,9 @@
+import "./Experience.css";
+function Experience() {
+    return (
+        <section id="Experience">
+            Experience
+        </section>
+    );
+}
+export default Experience;
