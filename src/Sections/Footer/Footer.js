c7bf9a93940b2b8a05bc30ff8e63d4587e054d6d
Tai Helsel <33849433+taihelsel@users.noreply.github.com>
2022-07-05 12:26:31 -0400

    updated footer link

--- src/Sections/Footer/Footer.js
+++ src/Sections/Footer/Footer.js
@@ -10,7 +10,7 @@ function Footer() {
                         d="M5 3.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm0 2.122a2.25 2.25 0 10-1.5 0v.878A2.25 2.25 0 005.75 8.5h1.5v2.128a2.251 2.251 0 101.5 0V8.5h1.5a2.25 2.25 0 002.25-2.25v-.878a2.25 2.25 0 10-1.5 0v.878a.75.75 0 01-.75.75h-4.5A.75.75 0 015 6.25v-.878zm3.75 7.378a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm3-8.75a.75.75 0 100-1.5.75.75 0 000 1.5z">
                     </path>
                 </svg>
-                <a href="https://github.com/taihelsel/portfolio2/fork" target="_blank">Fork and use this site for yourself</a>
+                <a href="https://github.com/taihelsel/portfolio2" target="_blank">Fork and use this site for yourself</a>
             </div>
         </footer>
     )
